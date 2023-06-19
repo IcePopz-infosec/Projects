6ac3ad9bb8a98cbf84a9d2a61fc5c55443b7b6be
IcePopz <48578217+IcePopz-infosec@users.noreply.github.com>
2023-06-19 16:16:24 -0500

    Create caesarcipher.py
    Simple caesar cipher

---
+++ caesarcipher.py
@@ -0,0 +1,25 @@
+def caesar_encrypt(plaintext, shift):
+    ciphertext = ""
+    for char in plaintext:
+        if char.isalpha():
+            ascii_offset = ord('A') if char.isupper() else ord('a')
+            shifted_char = chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+            ciphertext += shifted_char
+        else:
+            ciphertext += char
+    return ciphertext
+
+
+def caesar_decrypt(ciphertext, shift):
+    return caesar_encrypt(ciphertext, -shift)
+
+
+# Example usage
+plaintext = "Hello, World!"
+shift = 3
+
+encrypted_text = caesar_encrypt(plaintext, shift)
+print("Encrypted Text:", encrypted_text)
+
+decrypted_text = caesar_decrypt(encrypted_text, shift)
+print("Decrypted Text:", decrypted_text)
